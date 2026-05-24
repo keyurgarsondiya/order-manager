2c16c191c0389a6894a54a6d71e2b737c87e78bf
Keyur R Garsondiya <keyurpatel8118@gmail.com>
2026-05-25 00:07:01 +0530

    First Commit

---
+++ server/src/index.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI as string;
+// console.log('MONGO_URI:', MONGO_URI);
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ message: 'VERA API is running', success: true });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
